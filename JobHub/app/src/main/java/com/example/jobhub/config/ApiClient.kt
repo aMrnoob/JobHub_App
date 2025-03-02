86ae2046558045c80e56241e900cab88a09f4e88
Capybara <161992111+aMrnoob@users.noreply.github.com>
2025-03-02 20:04:51 +0700

    sua chuc nang quen mat khau, thay doi UI

--- JobHub/app/src/main/java/com/example/jobhub/config/ApiClient.kt
+++ JobHub/app/src/main/java/com/example/jobhub/config/ApiClient.kt
@@ -4,7 +4,8 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    private const val BASE_URL = "http://192.168.1.9:8080/"
+
+    private const val BASE_URL = "http://10.0.2.2:8080/"
 
     fun createRetrofit(): Retrofit {
         return Retrofit.Builder()
